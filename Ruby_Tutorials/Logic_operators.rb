7b1831dd12ec897d77d2c608396d52239064298d
EAdeveloper <eddeveloper@hotmail.com>
2017-03-12 20:58:19 -0400

    Logic operactors

---
+++ Ruby_Tutorials/Logic_operators.rb
@@ -0,0 +1,36 @@
+
+puts (10 > 5 ) && (5 < 8)
+# true
+puts (10 > 5 ) && (9 < 8)
+# false
+puts (5 > 10 ) || (10 > 5)
+# true
+puts (10 > 5 ) || (djksanj)
+# true
+puts (10 > 5 ) and (5 < 8)
+# true
+puts (10 > 5 ) or (4 < 5)
+# true
+puts !false
+# true
+puts !true
+# false
+puts (not true)
+# false
+puts  (not false)
+true
+
+=begin
+Logic order
+	1. **
+	2. !
+	3. * / %
+	4. + -
+	5. < > <= >=
+	6. == <==> !=
+	7. &&
+	8. ||
+	9. Asignation
+	10. not
+	11. or and
+=end
